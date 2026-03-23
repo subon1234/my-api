9e76235b7048fe09eec1e8286080c0036221bbdb
subon1234 <109028344+subon1234@users.noreply.github.com>
2026-03-23 22:08:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
-app.use(cors());
 
-// 🔥 IMPORTANT (health check route)
 app.get("/", (req, res) => {
-  res.send("My API Live 🚀");
+  res.send("API WORKING 🔥");
 });
 
-app.get("/test", (req, res) => {
-  res.json({ message: "API working" });
-});
+const PORT = process.env.PORT || 3000;
 
-// 🔥 VERY IMPORTANT (Render fix)
-const PORT = process.env.PORT;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
+app.listen(PORT, () => {
+  console.log("Running on port " + PORT);
 });
